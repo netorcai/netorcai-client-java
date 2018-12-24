2e19ea75b6a2776d27b55cc1d85963cec6377b86
Millian Poquet <millian.poquet@inria.fr>
2018-12-24 21:36:13 +0100

    [misc] code structure

---
+++ org/netorcai/example/HelloPlayer.java
@@ -0,0 +1,13 @@
+package org.netorcai.example;
+
+import org.json.JSONObject;
+import org.netorcai.Client;
+//import org.netorcai.message.*;
+
+public class HelloPlayer
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
